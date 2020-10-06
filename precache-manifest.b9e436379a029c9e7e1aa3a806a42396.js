7a1c0ccf6d1f71f1a1270e30f204c2a7f048e936
Carter Bian <carterbian32@gmail.com>
2020-10-06 00:17:20 -0700

    Updates

---
+++ precache-manifest.b9e436379a029c9e7e1aa3a806a42396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b744b00cb0f26ada138f63820da9eebb",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "79853b05ab5b5a038e09",
+    "url": "/starwars/static/css/main.3a88b1ba.chunk.css"
+  },
+  {
+    "revision": "c09d1f135b78a44fb37b",
+    "url": "/starwars/static/js/2.ff69d0a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars/static/js/2.ff69d0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79853b05ab5b5a038e09",
+    "url": "/starwars/static/js/main.be86bf4c.chunk.js"
+  },
+  {
+    "revision": "1f4c9aac975eb98c99e4",
+    "url": "/starwars/static/js/runtime-main.8af6987b.js"
+  }
+]);
